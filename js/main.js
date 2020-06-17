4c3060b8ce024affa9acd7f3dab36c8912637edd
kolyas238 <kolyas238@gmail.com>
2020-06-17 22:37:54 +0300

    вывод карточки объявления

--- js/main.js
+++ js/main.js
@@ -15,6 +15,10 @@ var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
+var PHOTO_WIDTH = 45;
+var PHOTO_HEIGHT = 40;
+var PHOTO_ALT = 'Фотография жилья';
+
 var map = document.querySelector('.map');
 
 // случайное число
@@ -41,6 +45,13 @@ var shuffleArray = function (items) {
     clonedItems[j] = temp;
   }
   return clonedItems;
+};
+
+var roomTypes = {
+  flat: 'Квартира',
+  bungalo: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
 };
 
 // генерация массива объявлений
@@ -109,6 +120,130 @@ var placeAds = function (ads) {
 };
 
 var generatedAds = generateAds(ADS_QUANTITY);
-map.appendChild(placeAds(generatedAds));
+// map.appendChild(placeAds(generatedAds));
+placeAds(generatedAds);
+
+// отрисовка карточек
+
+var cardTemplate = document.querySelector('#card')
+.content
+.querySelector('.map__card');
+
+var renderFeatures = function (element, features) {
+  element.innerHTML = '';
+  for (var i = 0; i < features.length; i++) {
+    var feature = document.createElement('li');
+    feature.classList.add('popup__feature');
+    feature.classList.add('popup__feature--' + features[i]);
+    element.appendChild(feature);
+  }
+};
+
+var renderPhotos = function (container, photos) {
+  container.innerHTML = '';
+
+  for (var i = 0; i < photos.length; i++) {
+    var photoItem = document.createElement('img');
+    photoItem.classList.add('popup__photo');
+    photoItem.src = photos[i];
+    photoItem.width = PHOTO_WIDTH;
+    photoItem.height = PHOTO_HEIGHT;
+    photoItem.alt = PHOTO_ALT;
+    container.appendChild(photoItem);
+  }
+};
+
+var switchRooms = function (rooms) {
+  var str = '';
+
+  switch (rooms) {
+    case 1:
+      str = '1 комната';
+      break;
+
+    case 100:
+      str = ' 100 комнат';
+      break;
+
+    default:
+      str = rooms + ' комнаты';
+  }
+  return str;
+};
+
+var switchGuests = function (guests) {
+  var str = '';
+
+  switch (guests) {
+    case 0:
+      break;
+
+    case 1:
+      str = ' для 1 гостя';
+      break;
+
+    default:
+      str = ' для ' + guests + ' гостей';
+  }
+  return str;
+};
+
+var renderCard = function (ad) {
+  var cardItems = cardTemplate.cloneNode(true);
+  var cardFeatures = cardItems.querySelector('.popup__features');
+  var cardPhotos = cardItems.querySelector('.popup__photos');
+
+  cardItems.querySelector('.popup__title').textContent = ad.offer.title;
+  cardItems.querySelector('.popup__description').textContent = ad.offer.description;
+  cardItems.querySelector('.popup__text--address').textContent = ad.offer.address;
+
+  if (ad.offer.price) {
+    cardItems.querySelector('.popup__text--price').textContent = ad.offer.price + '₽/ночь';
+  } else {
+    cardItems.querySelector('.popup__text--price').classList.add('visually-hidden');
+  }
+
+  if (ad.offer.type) {
+    cardItems.querySelector('.popup__type').textContent = roomTypes[ad.offer.type];
+  } else {
+    cardItems.querySelector('.popup__type').classList.add('visually-hidden');
+  }
+
+  if (ad.offer.rooms || ad.offer.guests) {
+    cardItems.querySelector('.popup__text--capacity').textContent = switchRooms(ad.offer.rooms) + switchGuests(ad.offer.guests);
+  } else {
+    cardItems.querySelector('.popup__text--capacity').classList.add('visually-hidden');
+  }
+
+  if (ad.offer.checkin || ad.offer.checkout) {
+    cardItems.querySelector('.popup__text--time').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
+  } else {
+    cardItems.querySelector('.popup__text--time').classList.add('visually-hidden');
+  }
+
+  if (ad.offer.features) {
+    renderFeatures(cardFeatures, ad.offer.features);
+  } else {
+    cardFeatures.classList.add('visually-hidden');
+  }
+
+  if (ad.offer.photos) {
+    renderPhotos(cardPhotos, ad.offer.photos);
+  } else {
+    cardPhotos.classList.add('visually-hidden');
+  }
+
+  if (ad.author.avatar) {
+    cardItems.querySelector('.popup__avatar').src = ad.author.avatar;
+  } else {
+    cardItems.querySelector('.popup__avatar').classList.add('visually-hidden');
+  }
+
+  return cardItems;
+};
+
+var mapFilters = document.querySelector('.map__filters-container');
+
+map.insertBefore(renderCard(generatedAds[0]), mapFilters);
 
 activateMap();
